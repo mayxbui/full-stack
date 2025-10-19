8ca70ab308ee4bde6466cdb7a2f47d64827c311f
Nguyen Bui <may.thaonguyen2205@gmail.com>
2025-10-18 20:13:33 -0400

    stateeventexample

---
+++ part1/state-event/src/main.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// PAGE RE-RENDERING
+// let counter=1
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// const refresh = () => {
+//   root.render(
+//     <App counter={counter}/>
+//   )
+// }
+
+// setInterval(() => {
+//   refresh()
+//   counter+=1
+// }, 1000)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App/>
+)
